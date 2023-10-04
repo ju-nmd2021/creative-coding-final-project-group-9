e9d9448e12fe3d1b7a5957dc6a416f3b79129bad
Essien Sampson <essien.sampson01@gmail.com>
2023-10-04 21:57:54 +0200

    Art generation implemented
    made it generate an art when generate button is clicked and also resets on trigger

--- art.js
+++ art.js
@@ -2,12 +2,19 @@ let colors = [];
 let gridSize = 5;
 let boxSize;
 let selectedColors = [];
+let artCanvas;
+let artGenerated = false;
 
 function setup() {
   createCanvas(500, 500);
   boxSize = width / gridSize;
   noLoop();
   generateRandomColors();
+
+  // create a separate canvas for art
+  artCanvas = createCanvas(500, 500);
+  artCanvas.position(0, 0);
+  artCanvas.style("z-index", "-1"); //places the art canvas behind the main canvas
 }
 
 function draw() {
@@ -47,21 +54,48 @@ function displayColorGrid() {
 }
 
 function mousePressed() {
-  let col = floor(mouseX / boxSize);
-  let row = floor(mouseY / boxSize);
-  let index = row * gridSize + col;
+  if (!artGenerated) {
+    let col = floor(mouseX / boxSize);
+    let row = floor(mouseY / boxSize);
+    let index = row * gridSize + col;
 
-  // checks if mouse is pressed within color grid
-  if (col >= 0 && col < gridSize && row >= 0 && row < gridSize - 1) {
-    if (selectedColors.includes(index)) {
-      selectedColors.splice(selectedColors.indexOf(index), 1);
-    } else {
-      selectedColors.push(index);
+    // checks if mouse is pressed within color grid
+    if (col >= 0 && col < gridSize && row >= 0 && row < gridSize - 1) {
+      if (selectedColors.includes(index)) {
+        selectedColors.splice(selectedColors.indexOf(index), 1);
+      } else {
+        selectedColors.push(index);
+      }
+      displayColorGrid();
     }
-    displayColorGrid();
   }
 }
 
 function generateArt() {
-  console.log("Art generated");
+  // create random art piece based on selected colors
+  artCanvas.clear();
+  artCanvas.background(255);
+
+  for (let i = 0; i < 10; i++) {
+    let randomIndex = floor(random(selectedColors.length));
+    let selectedColor = colors[selectedColors[randomIndex]];
+
+    // draw a random shape with seleected color
+    fill(selectedColor);
+    noStroke();
+
+    // random ellipse
+    let x = random(width);
+    let y = random(height);
+    let size = random(20, 100);
+    ellipse(x, y, size, size);
+  }
+  artGenerated = true;
+}
+
+function resetCanvas() {
+  artCanvas.clear();
+  selectedColors = [];
+  displayColorGrid();
+  artGenerated = false;
 }
